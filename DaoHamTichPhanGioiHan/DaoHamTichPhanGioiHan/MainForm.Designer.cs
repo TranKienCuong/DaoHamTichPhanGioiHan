8bb08e70b9c6504fbf77219e73831d0226422ab1
TranKienCuong <mathchessonline@gmail.com>
2017-06-11 18:22:59 +0700

    first commit

---
+++ DaoHamTichPhanGioiHan/DaoHamTichPhanGioiHan/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace DaoHamTichPhanGioiHan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.solveButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.diffTextBox2 = new System.Windows.Forms.TextBox();
+            this.diffTextBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.waitingLabel = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // solveButton
+            // 
+            this.solveButton.Location = new System.Drawing.Point(402, 18);
+            this.solveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(126, 39);
+            this.solveButton.TabIndex = 2;
+            this.solveButton.Text = "Tính";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.solveButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
+            this.tabControl.Location = new System.Drawing.Point(13, 18);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(379, 535);
+            this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.diffTextBox2);
+            this.tabPage1.Controls.Add(this.diffTextBox1);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(371, 504);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Đạo hàm";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // diffTextBox2
+            // 
+            this.diffTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diffTextBox2.Location = new System.Drawing.Point(134, 200);
+            this.diffTextBox2.Name = "diffTextBox2";
+            this.diffTextBox2.Size = new System.Drawing.Size(135, 27);
+            this.diffTextBox2.TabIndex = 3;
+            // 
+            // diffTextBox1
+            // 
+            this.diffTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diffTextBox1.Location = new System.Drawing.Point(67, 114);
+            this.diffTextBox1.Name = "diffTextBox1";
+            this.diffTextBox1.Size = new System.Drawing.Size(273, 27);
+            this.diffTextBox1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DaoHamTichPhanGioiHan.Properties.Resources.daoham;
+            this.pictureBox2.Location = new System.Drawing.Point(8, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(355, 155);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(371, 504);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tích phân";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 27);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(371, 504);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Giới hạn";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kết quả:";
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultPictureBox.Location = new System.Drawing.Point(400, 94);
+            this.resultPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(332, 455);
+            this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resultPictureBox.TabIndex = 1;
+            this.resultPictureBox.TabStop = false;
+            // 
+            // waitingLabel
+            // 
+            this.waitingLabel.AutoSize = true;
+            this.waitingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.waitingLabel.Location = new System.Drawing.Point(546, 39);
+            this.waitingLabel.Name = "waitingLabel";
+            this.waitingLabel.Size = new System.Drawing.Size(79, 18);
+            this.waitingLabel.TabIndex = 5;
+            this.waitingLabel.Text = "đang tính...";
+            this.waitingLabel.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 566);
+            this.Controls.Add(this.waitingLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.solveButton);
+            this.Controls.Add(this.resultPictureBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Đạo hàm - Tích phân - Giới hạn";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox diffTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox diffTextBox2;
+        private System.Windows.Forms.Label waitingLabel;
+    }
+}
+
---
+++ DaoHamTichPhanGioiHan/DaoHamTichPhanGioiHan/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DaoHamTichPhanGioiHan
+{
+    public partial class MainForm : Form
+    {
+        public string document;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayText(string text)
+        {
+            document += text;
+            document += @"  }
+                        \end{document}
+                        ";
+            File.WriteAllText("input.tex", document);
+
+            Process process = new Process();
+            process.StartInfo.FileName = "batch.bat";
+            process.StartInfo.Arguments = "input";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            process.WaitForExit();
+
+            resultPictureBox.ImageLocation = "output.png";
+
+            document = @"\documentclass[12pt,a4paper]{slides}
+                    \begin{document}
+                    {\huge
+                    ";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            document = @"\documentclass[12pt,a4paper]{slides}
+                    \begin{document}
+                    {\huge
+                    ";
+        }
+
+        private void solveButton_Click(object sender, EventArgs e)
+        {
+            waitingLabel.Visible = true;
+
+            switch (tabControl.SelectedIndex)
+            {
+                case 0: // differential
+                    DisplayText(@"\[ \frac{\partial Q}{\partial t} = \frac{\partial s}{\partial t} \]");
+                    break;
+                case 1: // integral
+                    DisplayText(@"\[ \int_{0}^{\pi} \ (sin x + cos x )\, dx = 2. \]");
+                    break;
+                case 2: // limit
+                    DisplayText(@"\[ \lim_{x \to a} \frac{f(x) - f(a)}{x - a}. \]");
+                    break;
+            }
+
+            waitingLabel.Visible = false;
+        }
+    }
+}
